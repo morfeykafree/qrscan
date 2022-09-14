e602871a44a2c908ca57a0aac7eafe8eee4d5f14
Alex Filippov <alex@quvis.com>
2022-09-14 10:44:59 +0300

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+export const DATA = [
+	{
+		id: '1',
+		label: 'Домой',
+		link: '/',
+	},
+	{
+		id: '2',
+		label: 'Прога',
+		link: '/about',
+	},
+	{
+		id: '3',
+		label: 'QR Сканер',
+		link: '/qrscan',
+		source: '',
+	},
+	{
+		id: '4',
+		label: 'QR Читалка',
+		link: '/qrreader',
+		source: '',
+	},
+];
